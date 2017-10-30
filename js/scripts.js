b527eac53935b161d521ef50aa5c49e0d9e7166a
Amy Churchwell and Nathan Barrett <nathan@>
2017-10-30 10:16:03 -0700

    add address object

--- js/scripts.js
+++ js/scripts.js
@@ -8,6 +8,11 @@ function Address(address, state, zip, phone){
   this.address = address;
   this.state = state;
   this.zip = zip;
+  this.phone = phone;
+}
+
+Address.prototype.fullAddress = function(){
+  return this.address + ", " + this.state + " " + this.zip;
 }
 
 Contact.prototype.fullName = function(){
@@ -28,6 +33,8 @@ $(document).ready(function(){
     var inputPhone =  $("input#phone").val();
 
     var newContact = new Contact(inputFirstName, inputLastName);
+    var newAddress = new Address(inputAddress, inputState, inputZip, inputPhone);
+
 
     $("ul#contacts").append("<li><span class='contact'>" + newContact.fullName() + "</span></li>");
 
@@ -36,6 +43,8 @@ $(document).ready(function(){
       $("show-contact h2").text(newContact.firstName);
       $(".first-name").text(newContact.firstName);
       $(".last-name").text(newContact.lastName);
+      $(".address").text(newAddress.fullAddress());
+      $(".phone").text(newAddress.phone);
     });
     $("input#first-name").val("");
     $("input#last-name").val("");
